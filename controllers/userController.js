32075209194851c4e2f336f07707887abab9aa3d
SATHAPANA\chhan.bunthong <chhan.bunthong@sathapana.com.kh>
2022-07-06 11:53:12 +0700

    [update] - add update user vlaidation

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const Config = require('../models/Config')
 const User = require('../models/User')
 const { failureMsg } = require('../constants/responseMsg')
 const { extractJoiErrors, readExcel, encryptPassword } = require('../helpers/utils')
-const { createUserValidation } = require('../middleware/validations/userValidation')
+const { createUserValidation, updateUserValidation } = require('../middleware/validations/userValidation')
 
 exports.index = (req, res) => {
     User.find({ isDisabled: false }, (err, users) => {
@@ -45,12 +45,16 @@ exports.create = (req, res) => {
 
 exports.update = async (req, res) => {
     const body = req.body
-    const { error } = createUserValidation.validate(body, { abortEarly: false })
+    const { error } = updateUserValidation.validate(body, { abortEarly: false })
     if (error) return response.failure(422, extractJoiErrors(error), res)
 
     try {
-        const password = await encryptPassword(body.password)
-        User.findByIdAndUpdate(req.params.id, { ...body, password }, (err, user) => {
+        if (body.password !== '') {
+            body.password = await encryptPassword(body.password)
+        } else {
+            delete body.password
+        }
+        User.findByIdAndUpdate(req.params.id, body, (err, user) => {
             if (err) {
                 switch (err.code) {
                     default:
